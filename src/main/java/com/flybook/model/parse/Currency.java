04535c507e48f3da7bfdcb5a3754f3d21695dcf3
AlanLg <alanilateur@gmail.com>
2024-12-09 11:41:40 +0100

    added missing getters and setters

--- src/main/java/com/flybook/model/parse/Currency.java
+++ src/main/java/com/flybook/model/parse/Currency.java
@@ -3,7 +3,11 @@ package com.flybook.model.parse;
 import jakarta.xml.bind.annotation.XmlAccessorType;
 import jakarta.xml.bind.annotation.XmlAccessType;
 import jakarta.xml.bind.annotation.XmlAttribute;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Currency {
 
